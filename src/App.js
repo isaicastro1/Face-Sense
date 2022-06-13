e20977bd71993d0932e91a10add7f75ca1a0bb9b
isaicastro1 <isaicstr@gmail.com>
2022-06-12 17:22:36 -0700

    Updating arrangement of files

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai'; 
+import './App.css'; 
+
+const app = new Clarifai.App({
+  apiKey: '9eed9aafb0d34cad88423d068110ae53'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  // If model is not working, try this instead
+  // .predict('c0c0ac362b03416da06ab3fa36fb58e3')
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+        },
+        function(err) {
+          //
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+         <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange = {this.onInputChange}
+          onButtonSubmit = {this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
+}
+
+export default App;
