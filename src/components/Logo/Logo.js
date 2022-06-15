3d6a522f11c19fafe66ac52137cc6dd4b07d5cdf
isaicastro1 <isaicstr@gmail.com>
2022-06-14 18:42:52 -0700

    Added Clarifai API with box surrounding faces
    !
    !
    !

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
+    <div style={{maxWidth: '200px', marginTop: '-80px'}} className='ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
           <img style={{paddingTop: '2px'}} alt='logo' src={brain}/>
