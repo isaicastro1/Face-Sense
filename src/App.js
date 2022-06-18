7615a474b75eb36c057a694a9623f2733223bc8f
isaicastro1 <isaicstr@gmail.com>
2022-06-18 07:58:45 -0700

    Added Sign In Feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Clarifai from 'clarifai'; 
@@ -30,8 +31,13 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
+  }
+
+  onRouteChange = () => {
+    this.setState({route: 'home'});
   }
   
   calculateFaceLocation = (data) => {
@@ -69,19 +75,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <Particles className='particles'
+        <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange = {this.onInputChange}
-          onButtonSubmit = {this.onButtonSubmit}
-        />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        {this.state.route === 'signin' 
+        ? <SignIn onRouteChange={this.onRouteChange} />
+        : <div>
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImageLinkForm 
+              onInputChange = {this.onInputChange}
+              onButtonSubmit = {this.onButtonSubmit}
+            />
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+          </div>
+        }
       </div>
-    );
+     );
   }
 }
 
