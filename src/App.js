3ab2a298fd8f057b51462b97c24063864fab102a
isaicastro1 <isaicstr@gmail.com>
2022-06-18 11:45:25 -0700

    Fixed signing in issue

--- src/App.js
+++ src/App.js
@@ -37,15 +37,6 @@ class App extends Component {
       isSignedIn: false
     }
   }
-
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      this.setState({isSignedIn: false})
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true})
-    }
-    this.setState({route: route});
-  }
   
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -79,14 +70,24 @@ class App extends Component {
       .catch(err => console.log('Oooops', err));
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, route, box, imageUrl } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
-        {this.state.route === 'home' 
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home' 
           ? <div>
               <Logo />
               <Rank />
@@ -94,10 +95,10 @@ class App extends Component {
                 onInputChange = {this.onInputChange}
                 onButtonSubmit = {this.onButtonSubmit}
               />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
             </div>
           : (
-              this.state.route === 'signin'
+              route === 'signin'
                 ? <SignIn onRouteChange={this.onRouteChange} />
                 : <Register onRouteChange={this.onRouteChange}/>
             )
