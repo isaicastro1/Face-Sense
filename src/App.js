34e72f1ded075a6268f280e8fbb86c5931eb13a8
Isai Castro <isaicstr@gmail.com>
2023-01-30 18:55:54 -0700

    Added ability to scan for multiple faces in each image

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import "./App.css";
 const initialState = {
   input: "",
   imageUrl: "",
-  box: {},
+  boxes: [],
   route: "signin",
   guest: false,
   isSignedIn: false,
@@ -45,21 +45,22 @@ class App extends Component {
   };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputimage");
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
+    return data.outputs[0].data.regions.map((face) => {
+      const clarifaiFace = face.region_info.bounding_box;
+      const image = document.getElementById("inputimage");
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height,
+      };
+    });
   };
 
-  displayFaceBox = (box) => {
-    this.setState({ box: box });
+  displayFaceBox = (boxes) => {
+    this.setState({ boxes: boxes });
   };
 
   onInputChange = (event) => {
@@ -133,7 +134,7 @@ class App extends Component {
   };
 
   render() {
-    const { isSignedIn, route, box, imageUrl, guest } = this.state;
+    const { isSignedIn, route, boxes, imageUrl, guest } = this.state;
     return (
       <div className="App">
         <ParticlesBg num={50} type="cobweb" bg={true} />;
@@ -149,7 +150,7 @@ class App extends Component {
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition box={box} imageUrl={imageUrl} />
+            <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
           </div>
         ) : route === "home" ? (
           <div>
@@ -162,7 +163,7 @@ class App extends Component {
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition box={box} imageUrl={imageUrl} />
+            <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
           </div>
         ) : route === "signin" ? (
           <SignIn
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './FaceRecognition.css';
+import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' src={imageUrl} width='500px' height = 'auto'  />
-        <div 
-          className='bounding-box' 
-          style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}> 
-          </div>
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img id="inputimage" src={imageUrl} width="500px" height="auto" />
+        {boxes.map((box) => (
+          <div
+            key={box.topRow}
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default FaceRecognition;
